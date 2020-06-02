9d98cc694fbf2e76d9350a77ae8bb74a5a997958
dan7sc <su7cheng@gmail.com>
2020-06-02 06:40:57 -0300

    Add solution to exercise 8.8: Authors view

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries'
 
 const Authors = (props) => {
+  const [authors, setAuthors] = useState([])
+  const result = useQuery(ALL_AUTHORS)
+
+  useEffect(() => {
+    if (result.data) {
+      setAuthors(result.data.allAuthors)
+    }
+  }, [result])
+
   if (!props.show) {
     return null
   }
 
-  const authors = []
+  if (result.loading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloProvider, ApolloClient, InMemoryCache, HttpLink
+} from '@apollo/client'
 import App from './App';
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    url: 'http:localhost:4000'
+  })
+})
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider  client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
